937cffeb31e485cb1c4528d6687d3bbb78b6b63d
Frank van Meurs <fvanmeurs@bol.com>
2018-12-01 06:54:21 +0100

    Proof of humanity

--- main.go
+++ main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"bufio"
+	"github.com/sirupsen/logrus"
+	"os"
+	"strconv"
+)
+
 func main() {
-	
+	n, err := solvePuzzle();
+	if err != nil {
+		logrus.Error(err)
+	}
+	logrus.Infof("Result: %d", n)
+}
+
+func solvePuzzle() (n int, err error) {
+	numbers, err := readLinesFromInputFile()
+	var sum int
+	for k, v := range numbers {
+		sum += v
+		logrus.Infof("n: %d, v:%d, s: %d", k, v, sum)
+	}
+	return sum, nil
+}
+
+func readLinesFromInputFile() ([]int, error) {
+	f, err := os.Open("input.txt")
+	defer func() {
+		if err := f.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	if err != nil {
+		return nil, err
+	}
+	var numbers []int
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		line := s.Text()
+		if number, err := strconv.Atoi(line); err != nil {
+			continue
+		} else {
+			numbers = append(numbers, number)
+		}
+	}
+	return numbers, nil
 }
